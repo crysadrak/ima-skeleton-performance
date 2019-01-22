47f31621defec7bf8dda19db35329cffe7931d71
jiri.fencl <jiri.fencl@firma.seznam.cz>
2019-01-22 22:03:44 +0100

    modified test to more complex structure

--- app/page/home/HomeView.jsx
+++ app/page/home/HomeView.jsx
@@ -1,18 +1,24 @@
 import AbstractComponent from 'ima/page/AbstractComponent';
 import PropTypes from 'prop-types';
 import React from 'react';
-import { Link, Image, Headline1 } from 'ima-ui-atoms';
+import { Link, Image, Headline1, List, ListItem, Paragraph } from 'ima-ui-atoms';
 
 const Profiler = React.unstable_Profiler;
 const classStateA = {
   classNameHeading: 'heading-class-one-a',
   classNameImage: 'image-class-one-a',
-  classNameLink: 'link-class-one-a'
+  classNameLink: 'link-class-one-a',
+  classNameParagraph: 'p-class-one-a',
+  classNameList: 'list-class-one-a',
+  classNameListItem: 'li-class-one-a'
 };
 const classStateB = {
   classNameHeading: 'heading-class-one-b',
   classNameImage: 'image-class-one-b',
-  classNameLink: 'link-class-one-b'
+  classNameLink: 'link-class-one-b',
+  classNameParagraph: 'p-class-one-b',
+  classNameList: 'list-class-one-b',
+  classNameListItem: 'li-class-one-b'
 };
 
 const updateTimes = [];
@@ -30,7 +36,8 @@ export default class HomeView extends AbstractComponent {
   onButtonClick(e) {
     this.setState({
       classState: this.state.classState === 'a' ? 'b' : 'a',
-      classes: this.state.classState === 'a' ? classStateB : classStateA
+      classes: this.state.classState === 'a' ? classStateB : classStateA,
+      listType: this.state.classState === 'a' ? 'ul' : 'ol'
     });
   }
 
@@ -38,18 +45,21 @@ export default class HomeView extends AbstractComponent {
     clearInterval(this._interval);
   }
 
-  onProfilerRender(conName, mode, actualTime, baseTime) {
-    console.log(conName, mode, actualTime, baseTime);
+  onProfilerRender(conName, mode, actualTime, baseTime, startTime, commitTime) {
+    console.log(conName, mode, actualTime, baseTime, startTime, commitTime);
 
     if (mode === 'update') {
-      updateTimes.push({ actualTime, baseTime });
+      updateTimes.push({ actualTime, baseTime, startTime, commitTime });
     }
 
     const arrLen = updateTimes.length;
     const avgAct = updateTimes.reduce((prevVal, curVal) => prevVal + curVal.actualTime, 0) / arrLen;
     const avgBase = updateTimes.reduce((prevVal, curVal) => prevVal + curVal.baseTime, 0) / arrLen;
+    const avgStart = updateTimes.reduce((prevVal, curVal) => prevVal + curVal.startTime, 0) / arrLen;
+    const avgCommit = updateTimes.reduce((prevVal, curVal) => prevVal + curVal.commitTime, 0) / arrLen;
+    const avgStartToCommit = updateTimes.reduce((prevVal, curVal) => prevVal + curVal.commitTime - curVal.startTime, 0) / arrLen;
 
-    console.log(`avgs of ${arrLen}:`, avgAct, avgBase);
+    console.log(`avgs of ${arrLen}:`, avgAct, avgBase, avgStart, avgCommit, avgStartToCommit);
   }
 
   onInterval() {
@@ -70,7 +80,8 @@ export default class HomeView extends AbstractComponent {
 
     this.state = {
       classState: 'a',
-      classes: classStateA
+      classes: classStateA,
+      listType: 'ul'
     };
   }
 
@@ -83,30 +94,37 @@ export default class HomeView extends AbstractComponent {
   }
 
   render() {
-    const { classNameHeading, classNameImage, classNameLink } = this.state.classes;
-    const components = [...Array(100)].map((x, i) => (
-      <div key={i} className="wrapper">
-        <Image
-          src={
-            this.utils.$Router.getBaseUrl() +
-            this.utils.$Settings.$Static.image +
-            '/imajs-logo.png'
-          }
-          alt="IMA.js logo"
-          className={classNameImage}
-        />
-        <Headline1 className={classNameHeading}>
-          {`${this.localize('home.hello')}, ${this.props.message} `}
-          <Link
-            className={classNameLink}
-            href="//imajs.io"
-            title={this.props.name}
-            target="_blank"
-            rel="noopener noreferrer">
-            {this.props.name}
-          </Link>
-        </Headline1>
-      </div>
+    const { classNameHeading, classNameImage, classNameLink, classNameList, classNameListItem, classNameParagraph } = this.state.classes;
+    const components = [...Array(30)].map((x, i) => (
+      <List key={i} className={classNameList} type={this.state.listType}>
+        <ListItem className={classNameListItem}>
+          <Image
+            src={
+              this.utils.$Router.getBaseUrl() +
+              this.utils.$Settings.$Static.image +
+              '/imajs-logo.png'
+            }
+            alt="IMA.js logo"
+            className={classNameImage}
+          />
+        </ListItem>
+        <ListItem className={classNameListItem}>
+          <Headline1 className={classNameHeading}>
+            {`${this.localize('home.hello')}, ${this.props.message} `}
+            <Link
+              className={classNameLink}
+              href="//imajs.io"
+              title={this.props.name}
+              target="_blank"
+              rel="noopener noreferrer">
+              {this.props.name}
+            </Link>
+          </Headline1>
+        </ListItem>
+        <ListItem className={classNameListItem}>
+          <Paragraph mode="14" className={classNameParagraph}>Lorem Ipsum</Paragraph>
+        </ListItem>
+      </List>
     ));
 
     return (
